e4420501d8c60395a8eacd76eee934c466d34e4e
Aslanov Rustam <aslanovrg@gmail.com>
2020-10-12 23:08:17 +0300

    добавляет функцию изменения темы

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './styles.css';
 import menuTempl from './templates/menu-temlate.hbs';
 import menu from './menu.json';
 
-const Theme = {
+const theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
@@ -15,5 +15,15 @@ const createMenuItem = createMenu(menu);
 const menuItem = document.querySelector('.menu.js-menu');
 menuItem.insertAdjacentHTML('beforeend', createMenuItem);
 
+const themeSwitch = document.querySelector('#theme-switch-toggle');
+themeSwitch.addEventListener('change', themeSelector);
 
+const bodyClass = document.querySelector('body');
+// const STORAGE_KEY = 'lightTheme';
+// const savedTheme = localStorage.getItem(STORAGE_KEY);
+bodyClass.classList.add(theme.LIGHT);
+function themeSelector(evt) {
+  bodyClass.classList.toggle(theme.LIGHT)
+  bodyClass.classList.toggle(theme.DARK)
+}
 
