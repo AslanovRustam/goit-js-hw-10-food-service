80f5963628e14e58281fe6acc68e6107bfce5fc0
Aslanov Rustam <aslanovrg@gmail.com>
2020-10-12 21:05:21 +0300

    создвет шаблон для отображения ингридиентов

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import './styles.css';
 import menuTempl from './templates/menu-temlate.hbs';
 import menu from './menu.json';
 
-// console.log(menuTempl(menu[2]))
-function createMenu(menu) {
-    // return menu.map(menuItem => menuTempl(menuItem))
-return menu.map(menuTempl).join('');
-}
-const createMenuItem = createMenu(menu);
-const menuItem = document.querySelector('.menu.js-menu');
-menuItem.insertAdjacentHTML('beforeend', createMenuItem);
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
+
+function createMenu(menu) {
+    // return menu.map(menuItem => menuTempl(menuItem))
+  return menu.map(menuTempl).join('');
+}
+const createMenuItem = createMenu(menu);
+const menuItem = document.querySelector('.menu.js-menu');
+menuItem.insertAdjacentHTML('beforeend', createMenuItem);
+
+
+
