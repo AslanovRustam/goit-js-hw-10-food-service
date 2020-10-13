b61bbf5b21ef98925539e4b1cb20719f8bcc19cd
Aslanov Rustam <aslanovrg@gmail.com>
2020-10-13 23:14:09 +0300

    сохраняет текущее состояние темы

--- src/index.js
+++ src/index.js
@@ -19,11 +19,19 @@ const themeSwitch = document.querySelector('#theme-switch-toggle');
 themeSwitch.addEventListener('change', themeSelector);
 
 const bodyClass = document.querySelector('body');
-// const STORAGE_KEY = 'lightTheme';
-// const savedTheme = localStorage.getItem(STORAGE_KEY);
+const STORAGE_KEY = 'light-theme';
+// const startTheme = localStorage.getItem(STORAGE_KEY);
 bodyClass.classList.add(theme.LIGHT);
 function themeSelector(evt) {
   bodyClass.classList.toggle(theme.LIGHT)
   bodyClass.classList.toggle(theme.DARK)
+
+  if (bodyClass.classList.contains(STORAGE_KEY)) {
+  localStorage.setItem('STORAGE_KEY', JSON.stringify(''))
+  localStorage.setItem('STORAGE_KEY', JSON.stringify('light-theme'))
+  } else if (!bodyClass.classList.contains(STORAGE_KEY)) {
+    localStorage.setItem('STORAGE_KEY', JSON.stringify(''))
+    localStorage.setItem('STORAGE_KEY', JSON.stringify('dark-theme'))
+  }
 }
 
