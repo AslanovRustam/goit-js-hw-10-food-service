5399a1267fb2ec409eafb6eab2b6559fa9dca2d0
Aslanov Rustam <aslanovrg@gmail.com>
2020-10-15 00:29:33 +0300

    добавляет функцию checked к ползунку и убирает обновление станицы до исходного состояния при перезагрузке

--- src/index.js
+++ src/index.js
@@ -21,18 +21,33 @@ themeSwitch.addEventListener('change', themeSelector);
 const bodyClass = document.querySelector('body');
 const STORAGE_KEY = 'light-theme';
 // const startTheme = localStorage.getItem(STORAGE_KEY);
-bodyClass.classList.add(theme.LIGHT);
+
 function themeSelector(evt) {
   bodyClass.classList.toggle(theme.LIGHT)
   bodyClass.classList.toggle(theme.DARK)
 
   if (bodyClass.classList.contains(STORAGE_KEY)) {
-  localStorage.setItem('STORAGE_KEY', JSON.stringify(''))
-  localStorage.setItem('STORAGE_KEY', JSON.stringify('light-theme'))
+  localStorage.removeItem(STORAGE_KEY);
+  localStorage.setItem(STORAGE_KEY, true)
   } else if (!bodyClass.classList.contains(STORAGE_KEY)) {
-    localStorage.setItem('STORAGE_KEY', JSON.stringify(''))
-    localStorage.setItem('STORAGE_KEY', JSON.stringify('dark-theme'))
+      localStorage.removeItem(STORAGE_KEY);
+    localStorage.setItem(STORAGE_KEY, false)
   }
+}
+
+const savedTheme = localStorage.getItem(STORAGE_KEY)
+
+if (savedTheme === '') {
+  bodyClass.classList.add(theme.LIGHT);
+}
+if (savedTheme === 'false') {
+  bodyClass.classList.remove(theme.LIGHT);
+  bodyClass.classList.add(theme.DARK);
+  themeSwitch.checked = true;
+}
+if (savedTheme === 'true') {
+    bodyClass.classList.remove(theme.DARK);
+  bodyClass.classList.add(theme.LIGHT);
 }
 
 
